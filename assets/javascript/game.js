75e2aa89ff69af81c5fac27b929990469b13aaf2
Tony C <tonychiesa92@gmail.com>
2019-06-14 14:48:59 -0400

    added comments to .js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,33 @@
+// game.js
+
+// define variables
 var alpabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = alpabetArray[Math.floor(Math.random() * alpabetArray.length)];
 var wins = 0;
 var losses = 0;
 var count = 9;
 var guess = "";
 
-
+// define elements by id
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var countText = document.getElementById("count-text");
 var guessesText = document.getElementById("guesses-text");
 
-
-var computerGuess = alpabetArray[Math.floor(Math.random() * alpabetArray.length)];
-
+// get user input
 document.onkeyup = function (event) {
     var userGuess = event.key;
+
+    // checking for duplicate entries
     var found = guess.search(userGuess);
     if ((found === -1)) {
-
+        // not a duplicate entry
         guess = guess + " " + userGuess;
 
-
+        // checking guess
         if ((userGuess !== computerGuess)) {
             count--;
+            // incorect guess
             if ((count === 0)) {
                 losses++;
                 count = 9;
@@ -31,20 +36,16 @@ document.onkeyup = function (event) {
             }
         }
         else {
+            // correct guess
             wins++;
             count = 9;
             guess = "";
             computerGuess = alpabetArray[Math.floor(Math.random() * alpabetArray.length)];
         }
-
-
+        // display to html
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses: " + losses;
         countText.textContent = "Guesses left: " + count;
         guessesText.textContent = "Your guesses so far: " + guess;
-
-
     }
 }
-
-
