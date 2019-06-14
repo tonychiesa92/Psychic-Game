63b99a5ecdcfd9a8293e9955224667dc693a0885
Tony C <tonychiesa92@gmail.com>
2019-06-14 14:40:00 -0400

    javascript logic figured out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,50 @@
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+var alpabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var count = 9;
+var guess = "";
+
+
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var countText = document.getElementById("count-text");
+var guessesText = document.getElementById("guesses-text");
+
+
+var computerGuess = alpabetArray[Math.floor(Math.random() * alpabetArray.length)];
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    var found = guess.search(userGuess);
+    if ((found === -1)) {
+
+        guess = guess + " " + userGuess;
+
+
+        if ((userGuess !== computerGuess)) {
+            count--;
+            if ((count === 0)) {
+                losses++;
+                count = 9;
+                guess = "";
+                computerGuess = alpabetArray[Math.floor(Math.random() * alpabetArray.length)];
+            }
+        }
+        else {
+            wins++;
+            count = 9;
+            guess = "";
+            computerGuess = alpabetArray[Math.floor(Math.random() * alpabetArray.length)];
+        }
+
+
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        countText.textContent = "Guesses left: " + count;
+        guessesText.textContent = "Your guesses so far: " + guess;
+
+
     }
-    return result;
- }
- 
- console.log(makeid(1));
+}
+
+
